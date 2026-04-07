603d7068878d113b7407e31303417a5d5f5094f8
darkmatter <vikasanand.darkmatter@gmail.com>
2026-04-07 15:42:27 +0530

    Minor changes

--- Frontend/src/Constant.js
+++ Frontend/src/Constant.js
@@ -1,3 +1,3 @@
-// export const AUTH_URL = 'https://btp-auth-server-8dnd.onrender.com';
-export const AUTH_URL = 'http://localhost:3000';
+export const AUTH_URL = 'https://btp-auth-server-8dnd.onrender.com';
+// export const AUTH_URL = 'http://localhost:3000';
 export const SOCKET_URL = 'http://localhost:8000';
